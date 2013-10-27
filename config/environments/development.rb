b377d6d00611ffc467bcba7f451f2f1c397d0dc9
Michelle Lara Lin <michelle@michellelaralin.com>
2013-10-27 15:36:47 -0400

    omfg fucking precompile the shit with the fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,9 @@ ElevatorPitch::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+ 
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
 
 end
