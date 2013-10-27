2d0fc3e86c89ea145e6e473eefa55ebc865a40c5
Michelle Lara Lin <michelle@michellelaralin.com>
2013-10-27 14:24:07 -0400

    add fonts pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ ElevatorPitch::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 end
